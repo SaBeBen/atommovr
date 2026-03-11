30a2796cf2b07953b8525ed49d8e81eb6b742711
ntuphys.mattchen.page <129600488+phys-mattchen@users.noreply.github.com>
2026-03-11 14:53:29 -0500

    Delete atommover directory

--- atommover/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from atommover.utils.core import ArrayGeometry, Configurations, PhysicalParams
-from atommover.utils.aod_timing import _get_decel_putdown_flags, _get_pickup_accel_flags, _classify_new_and_continuing_tones
-from atommover.utils.animation import single_species_image, dual_species_image, make_single_species_gif, make_dual_species_gif
-from atommover.utils.move_utils import MoveType, move_atoms, move_atoms_noiseless, get_AOD_cmds_from_move_list, get_move_list_from_AOD_cmds
-from atommover.utils.Move import Move, FailureEvent, FailureFlag
-from atommover.utils.AtomArray import AtomArray
-from atommover.utils.ErrorModel import ErrorModel
-from atommover.utils.errormodels import UniformVacuumTweezerError, ZeroNoise
-from atommover.utils.benchmarking import Benchmarking, BenchmarkingFigure
